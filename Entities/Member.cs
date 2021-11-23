7460976cad421e9e9f857cf7f6fe83b7a87b72a8
Matías Salinas <salinasmatias1990@gmail.com>
2021-11-23 20:49:14 -0300

    Added Membber Entity

---
+++ Entities/Member.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Member
+    {
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [StringLength(255)]
+        public string FacebookUrl { get; set; }
+
+        [StringLength(255)]
+        public string InstagramUrl { get; set; }
+
+        [StringLength(255)]
+        public string LinkedinUrl { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string Image { get; set; }
+
+        [StringLength(255)]
+        public string Description { get; set; }
+    }
+}
