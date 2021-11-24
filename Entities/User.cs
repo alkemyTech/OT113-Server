5e9626d2cda723eb503e1c2990ed1ca81ea99222
Nicolas Cordoba <nicolas.ncordoba@gmail.com>
2021-11-24 11:03:21 -0300

    agrego namespace

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,29 +1,34 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
-public class User : EntityBase
-{
 
-	[Required]
-	[MaxLenght(255)]
-	public string firstName { get; set; }
+namespace OT113-Server.Entities {
 
-	[Required]
-	[MaxLength(255)]
-	public string lastName { get; set; }
+	public class User : EntityBase
+	{
 
-	[Required]
-	[MaxLenght(320)]
-	public string Email { get; set; }
+		[Required]
+		[MaxLenght(255)]
+		public string firstName { get; set; }
 
-	[Required]
-	[MaxLength(20)]
-	public string Password { get; set; }
+		[Required]
+		[MaxLength(255)]
+		public string lastName { get; set; }
 
-	[MaxLength(255)]
-	public string Photo { get; set; }
+		[Required]
+		[MaxLenght(320)]
+		public string Email { get; set; }
 
-	[ForeignKey("Role")]
-	public int    roleId { get; set; }
+		[Required]
+		[MaxLength(20)]
+		public string Password { get; set; }
+
+		[MaxLength(255)]
+		public string Photo { get; set; }
+
+		[ForeignKey("Role")]
+		public int roleId { get; set; }
+
+	}
 
 }
