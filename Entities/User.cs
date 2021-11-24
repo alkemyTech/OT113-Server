99076c01aa3e78b755b7025b7f542266037c3bff
Nicolas Cordoba <nicolas.ncordoba@gmail.com>
2021-11-24 09:56:31 -0300

    creo entity User

---
+++ Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class User : EntityBase
+{
+
+	[Required]
+	[MaxLenght(255)]
+	public string firstName { get; set; }
+
+	[Required]
+	[MaxLength(255)]
+	public string lastName { get; set; }
+
+	[Required]
+	[MaxLenght(320)]
+	public string Email { get; set; }
+
+	[Required]
+	[MaxLength(20)]
+	public string Password { get; set; }
+
+	[MaxLength(255)]
+	public string Photo { get; set; }
+
+	[ForeignKey("Role")]
+	public int    roleId { get; set; }
+
+}
