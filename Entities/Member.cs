5cdb306e7384c1abe3d49c64d60379cbdb77a166
Matías Salinas <salinasmatias1990@gmail.com>
2021-11-23 21:55:45 -0300

    Fixed issues with properties as requested on pull request

--- Entities/Member.cs
+++ Entities/Member.cs
@@ -8,33 +8,28 @@ using System.Threading.Tasks;
 
 namespace Entities
 {
-    public class Member : IEntityBase
+    public class Member : EntityBase
     {
-        [Key]
-        public int Id { get; set; }
-
+        
         [Required]
-        [StringLength(255)]
+        [MaxLength(255)]
         public string Name { get; set; }
 
-        [StringLength(255)]
+        [MaxLength(255)]
         public string FacebookUrl { get; set; }
 
-        [StringLength(255)]
+        [MaxLength(255)]
         public string InstagramUrl { get; set; }
 
-        [StringLength(255)]
+        [MaxLength(255)]
         public string LinkedinUrl { get; set; }
 
-        [StringLength(255)]
+        [MaxLength(255)]
         [Required]
         public string Image { get; set; }
 
-        [StringLength(255)]
+        [MaxLength(255)]
         public string Description { get; set; }
 
-        [Required]
-        public bool isDelete { get; set; }
-        public DateTime modifiedAt { get; set; }
     }
 }
