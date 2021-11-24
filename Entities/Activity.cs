5bf396f1b1709f4d062e4f8dd8e9d98f493355f0
Matías Salinas <salinasmatias1990@gmail.com>
2021-11-23 21:38:17 -0300

    Defined properties for Activity Entity

--- Entities/Activity.cs
+++ Entities/Activity.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Abstractions;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,16 @@ namespace Entities
 {
     public class Activity
     {
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "ntext(65535)")]
+        public string Content { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Image { get; set; }
 
     }
 }
