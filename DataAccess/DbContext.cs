7b1398bf20d30aedab1205904e6961a0d746aabc
Matías Salinas <salinasmatias1990@gmail.com>
2021-11-25 14:21:36 -0300

    Added interfaces Icrud, IDbContext, IRepository and their respective implementations

---
+++ DataAccess/DbContext.cs
@@ -0,0 +1,38 @@
+﻿using Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class DbContext<T> : IDbContext<T> where T : class, IEntityBase
+    {
+        DbSet<T> _items;
+        ApiDbContext _ctx;
+        public DbContext(ApiDbContext ctx)
+        {
+            _ctx = ctx;
+            _items = ctx.Set<T>();
+        }
+        public void Delete(int id)
+        {
+        }
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _items.ToListAsync();
+        }
+        public T GetById(int id)
+        {
+            return _items.Where(i => i.Id.Equals(id)).FirstOrDefault();
+        }
+        public T Save(T entity)
+        {
+            _items.Add(entity);
+            _ctx.SaveChanges();
+            return entity;
+        }
+    }
+}
---
+++ Repositories/Repository.cs
@@ -0,0 +1,37 @@
+﻿using Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface IRepository<T> : ICrud<T>
+    {
+    }
+    public class Repository<T> : IRepository<T> where T : IEntityBase
+    {
+        IDbContext<T> _ctx;
+        public Repository(IDbContext<T> ctx)
+        {
+            _ctx = ctx;
+        }
+        public void Delete(int id)
+        {
+            _ctx.Delete(id);
+        }
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _ctx.GetAll();
+        }
+        public T GetById(int id)
+        {
+            return _ctx.GetById(id);
+        }
+        public T Save(T entity)
+        {
+            return _ctx.Save(entity);
+        }
+    }
+}
