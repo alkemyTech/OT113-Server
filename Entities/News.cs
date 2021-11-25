1e8b0dbe6b3b39a96d15dc4d09c10a1ec090d8d3
sergio351 <velazquezsergio714@mail.com>
2021-11-25 09:56:25 -0300

    added model for news

--- Entities/News.cs
+++ Entities/News.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using OT113;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities
 {
-    class News
+   public  class News : EntityBase
     {
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+
+        [Required]
+        [MaxLength(255)]
+        public string Image { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "ntext(65535)")]
+
+        public string Content { get; set; }
+
+
+        public Category Category { get; set; }
+
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+
     }
 }
