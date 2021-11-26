cb26bd0fb24e538a2d26a03f3acbf4f83eb828ee
IgnacioTotino <ignaciotomas.t@gmail.com>
2021-11-26 04:35:24 -0300

    Agregando entidad de slides

---
+++ Entities/Slides.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class Slides : EntityBase
+    {
+        public string ImgUrl { get; set; }
+
+        public string Text { get; set; }
+
+        public int Order { get; set; }
+
+        public int OrganizationId { get; set; }
+    }
+}
