f255b5d1e2cc79c8e7c0fa6aad8da5d7dd0e0d90
Nicolas Cordoba <nicolas.ncordoba@gmail.com>
2021-11-26 15:13:50 -0300

    Creo repositorio, controlador y servicio de Users

--- Core/Business/UserBusiness.cs
+++ Core/Business/UserBusiness.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Entities;
+using Repositories;
+using Core.Business.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,23 @@ using System.Threading.Tasks;
 
 namespace Core.Business
 {
-    public class UserBusiness
+    public class UserBusiness : IUserBusiness
     {
+        private readonly Repository<User> _repository;
+
+        public NewsBusiness(Repository<User> repository)
+        {
+            _repository = repository;
+        }
+
+        
+        public void AddUser() { }
+        public void RemoveUser(int id) { }
+        public void UpdateUser(News news) { }
+
+        public User GetUserById() { }
+        public async Task<IEnumerable<User>> GetAllUsers() { }
+    }
+
     }
 }
