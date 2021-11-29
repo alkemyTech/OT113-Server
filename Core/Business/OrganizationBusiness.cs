f134ee4e024be576903b1e8d7bb6d2cd16d6ac22
IgnacioTotino <ignaciotomas.t@gmail.com>
2021-11-28 21:46:24 -0300

    agregando metodos CRUD e interface de organizacion

---
+++ Core/Business/OrganizationBusiness.cs
@@ -0,0 +1,37 @@
+﻿using Core.Business.Interfaces;
+using Entities;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Business
+{
+    public class OrganizationBusiness : IOrganizationBusiness
+    {
+        private readonly IRepository<Organization> _organization;
+
+        public OrganizationBusiness (IRepository<Organization> repository)
+        {
+            _organization = repository;
+        }
+
+        public Organization GetOrganizationId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Organization>> GetAllOrganization()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrganization(Organization context) { }
+
+        public void DeleteOrganization(int id) { }
+
+        public void InsertOrganization() { }
+    }
+}
