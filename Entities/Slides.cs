ce69fed9abae9df973f7d025ce453d701805b585
IgnacioTotino <ignaciotomas.t@gmail.com>
2021-11-28 21:25:42 -0300

    agregando controlador y entidad de slides

--- Entities/Slides.cs
+++ Entities/Slides.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Entities
 {
-    class Slides
+    class Slides : EntityBase
     {
+        public string ImgUrl { get; set; }
+
+        public string Text { get; set; }
+
+        public int Order { get; set; }
+
+        public int OrganizationId { get; set; }
     }
 }
