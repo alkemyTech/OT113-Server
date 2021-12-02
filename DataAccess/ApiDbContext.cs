54bfdbeeb47d30722fe9e03448c29f527778b582
IgnacioTotino <ignaciotomas.t@gmail.com>
2021-12-02 20:50:07 -0300

    Agregando varios seeds

--- DataAccess/ApiDbContext.cs
+++ DataAccess/ApiDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Abstractions;
+using Core.Helper;
 using Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -22,8 +23,160 @@ namespace DataAccess
         public DbSet<Slides> Slides { get; set; }
         public DbSet<Testimonials> Testimonials { get; set; }
         public DbSet<User> Users { get; set; }
+
         public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            SeedUsers(modelBuilder);
+            SeedNews(modelBuilder);
+            SeedActivity(modelBuilder);
+            SeedMembres(modelBuilder);
+            SeedTestimonals(modelBuilder);
+            SeedCategories(modelBuilder);
+        }
+      
+        private void SeedUsers(ModelBuilder modelBuilder)
+        {             
+            modelBuilder.Entity<User>().HasData(new User()
+            {
+                Id = 1,
+                firstName = "TestNameUser",
+                lastName = "TestLastNameUser",
+                Email = "Test@email.user",
+                Password = "TestLastPassUser",
+                Photo = "TestPhoto",
+                roleId = 1,
+                isDelete = false,
+                modifiedAt = DateTime.Now
+
+            },new User()
+            {
+                Id = 2,
+                firstName = "TestNameUser",
+                lastName = "TestLastNameUser",
+                Email = "Test@email.user",
+                Password = "TestLastPassUser",
+                Photo = "TestPhoto",
+                roleId = 2,
+                isDelete = false,
+                modifiedAt = DateTime.Now
+
+            },new User() 
+            {
+                Id = 3,
+                firstName = "TestNameUser",
+                lastName = "TestLastNameUser",
+                Email = "Test@email.user",
+                Password = "TestLastPassUser",
+                Photo = "TestPhoto",
+                roleId = 1,
+                isDelete = false,
+                modifiedAt = DateTime.Now
+            }); 
+        }
+
+        private void SeedNews(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<News>().HasData(new News()
+            {
+                Id = 1,
+                Name = "TestNameNews",
+                Content = "TestContentNews",
+                Image = "TestImageNews",
+                CategoryId = 1,
+                isDelete = false,
+                modifiedAt = DateTime.Now
+
+            }, new News()
+            {
+                Id = 2,
+                Name = "TestNameNews",
+                Content = "TestContentNews",
+                Image = "TestImageNews",
+                CategoryId = 2,
+                isDelete = false,
+                modifiedAt = DateTime.Now
+
+            }, new News()
+            {
+                Id = 3,
+                Name = "TestNameNews",
+                Content = "TestContentNews",
+                Image = "TestImageNews",
+                CategoryId = 1,
+                isDelete = false,
+                modifiedAt = DateTime.Now
+            });
+        }
+
+        private void SeedActivity(ModelBuilder modelBuilder)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                modelBuilder.Entity<Activity>().HasData(new Activity
+                {
+                    Id = i,
+                    Name = "TestNamegit",
+                    Content = "TestContent",
+                    Image = "TestImage",
+                    isDelete = false
+                });
+            };
+        }
+
+        private void SeedMembres(ModelBuilder modelBuilder)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                modelBuilder.Entity<Member>().HasData(new Member
+                {   
+
+                    Id = i,
+                    Name = "TestNameMembers",
+                    FacebookUrl = "www.TestUrl.Members1",
+                    InstagramUrl = "www.TestUrl.Members2",
+                    LinkedinUrl = "www.TestUrl.Members3",
+                    Image = "TestImageMembers",
+                    Description = "TestDescriptionMembers",
+                    isDelete = false,
+                    modifiedAt = DateTime.Now
+
+                });
+            };
+        }
+
+        private void SeedTestimonals(ModelBuilder modelBuilder)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                modelBuilder.Entity<Testimonials>().HasData(new Testimonials()
+                {
+                    Id = i,
+                    Name = "TestNameTestimonals",
+                    Content = "TestContentTestimonals",
+                    Image = "TestImageTestimonals",
+                    modifiedAt = DateTime.Now
+                });
+            }
+        }
+
+        private void SeedCategories(ModelBuilder modelBuilder)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                modelBuilder.Entity<Category>().HasData(new Category
+                {
+                    Id = i,
+                    Name = "TestNameCategory",
+                    Description = "TestContentCategory",
+                    Image = "TestImageCategory",
+                    isDelete = false,
+                    modifiedAt = DateTime.Now
+                });
+            }
         }
     }
 }
