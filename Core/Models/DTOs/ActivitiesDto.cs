dc22eab0df75f785a67d5e6dbddd900302b810bd
IgnacioTotino <ignaciotomas.t@gmail.com>
2021-12-07 17:00:16 -0300

    Agregando dtos

---
+++ Core/Models/DTOs/ActivitiesDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models.DTOs
+{
+    public class ActivitiesDto
+    {   
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Content { get; set; }
+    }
+}
