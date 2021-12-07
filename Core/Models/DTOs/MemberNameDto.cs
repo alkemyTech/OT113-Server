35d3a4d93eb0efc56ef48924b44c9e98b895808b
IgnacioTotino <ignaciotomas.t@gmail.com>
2021-12-07 17:38:53 -0300

    x

--- Core/Models/DTOs/MemberNameDto.cs
+++ Core/Models/DTOs/MemberNameDto.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Core.Models.DTOs
 {
-    class MemberNameDto
+    public class MemberNameDto
     {
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
     }
 }
--- Core/Models/DTOs/TestimonailsDto.cs
+++ Core/Models/DTOs/TestimonailsDto.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Core.Models.DTOs
 {
-    class TestimonailsDto
+    public class TestimonailsDto
     {
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [MaxLength(65535)]
+        public string Content { get; set; }
     }
 }
