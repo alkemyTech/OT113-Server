18eb5e8c3831e4207778f0eadcce5e737c0b8613
Nicolas Cordoba <nicolas.ncordoba@gmail.com>
2021-12-20 18:33:47 -0300

    fix nombre de img category en post y put, e inclusion de img en post y put de testimonial

---
+++ Core/Mapper/exceptblanks.cs
@@ -0,0 +1,20 @@
+using System.Text;
+
+namespace Core.Mapper
+{
+    public static class exceptblanks
+    {
+        
+        public static string ExceptBlanks(this string str)
+        {
+            StringBuilder sb = new StringBuilder(str.Length);
+            for (int i = 0; i < str.Length; i++)
+            {
+                char c = str[i];
+                if(!char.IsWhiteSpace(c))
+                    sb.Append(c);
+            }
+            return sb.ToString();
+        }
+    }
+}
