cca4ce4eded8508d58ee7d774ea5cc5800992a5c
Nicolas Cordoba <nicolas.ncordoba@gmail.com>
2021-12-29 20:55:39 -0300

    unit test endpoints Organization Controller

---
+++ Test/Helper/OrganizationBusinessTest.cs
@@ -0,0 +1,118 @@
+﻿using Core.Business.Interfaces;
+using Core.Mapper;
+using Core.Models.DTOs;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Helper
+{
+    class OrganizationBusinessTest : IOrganizationBusiness
+    {
+        private const string img = "Image\\Captura1.png";
+        private readonly List<Organization> _organizations;
+
+        public OrganizationBusinessTest()
+        {
+ 
+            _organizations = new List<Organization>()
+            {
+                new Organization() {
+                    Id = 1, 
+                    Name = "TestOrg1",
+                    Image = img,
+                    Address = "testAdressOrg1",
+                    Phone = "0303456nanana",
+                    Email = "test@testOrg1.com",
+                    WelcomeText = "welcome text org test 1",
+                    AboutUsText = "about us text org test 1",
+                    Facebook = "www.facebook.com/testOrg1",
+                    Linkedin = "www.linkedin.com/testOrg1",
+                    Instagram = "www.instagram.com/testOrg1",
+                    isDelete = false,
+                    modifiedAt = DateTime.Now
+                },
+                new Organization() {
+                    Id = 2,
+                    Name = "TestOrg2",
+                    Image = "",
+                    Address = "testAdressOrg2",
+                    Phone = "0303456nanana2",
+                    Email = "test@testOrg2.com",
+                    WelcomeText = "welcome text org test 2",
+                    AboutUsText = "about us text org test 2",
+                    Facebook = "www.facebook.com/testOrg2",
+                    Linkedin = "www.linkedin.com/testOrg2",
+                    Instagram = "www.instagram.com/testOrg2",
+                    isDelete = true,
+                    modifiedAt = DateTime.Now
+                }
+            };
+        }
+
+        public OrganizationDto GetById(int id)
+        {
+            
+             var organization = _organizations.Where(o => o.Id == id).FirstOrDefault();
+
+            if (organization != null)
+            {
+                var organizationDto = new OrganizationDto
+                {
+                    Name = organization.Name,
+                    Image = organization.Image,
+                    Address = organization.Address,
+                    Phone = organization.Phone,
+                    Facebook = organization.Facebook,
+                    Linkedin = organization.Linkedin,
+                    Instagram = organization.Instagram,
+                    Slides = new List<SlidesDTO>()
+                };
+
+                return organizationDto;
+            }
+           
+            return null;
+            
+        }
+
+        public Organization GetOrg(int id)
+        {
+            var organization = _organizations.Where(o => o.Id == id).FirstOrDefault();
+
+            if (organization != null)
+            {
+                return organization;
+            }
+
+            return null;
+
+        }
+
+        public void UpdateOrganization(OrganizationDtoPostRequest organization)
+        {
+          
+            var organizationEdit = GetOrg(organization.Id);
+            var organizationRemove = organizationEdit;
+            _organizations.Remove(organizationRemove);
+
+            organizationEdit.Name = organization.Name;
+            organizationEdit.Image = organization.Image;
+            organizationEdit.Address = organization.Address;
+            organizationEdit.Phone = organization.Phone;
+            organizationEdit.Facebook = organization.Facebook;
+            organizationEdit.Linkedin = organization.Linkedin;
+            organizationEdit.Instagram = organization.Instagram;
+            organizationEdit.WelcomeText = organization.WelcomeText;
+            organizationEdit.AboutUsText = organization.AboutUsText;
+            organizationEdit.isDelete = false;
+            organizationEdit.modifiedAt = DateTime.Now;
+
+            _organizations.Add(organizationEdit);
+           
+        }
+    }
+}
---
+++ Test/OrganizationControllerTest.cs
@@ -0,0 +1,102 @@
+﻿using Core.Business.Interfaces;
+using Core.Models.DTOs;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OngProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Helper;
+
+namespace Test
+{
+    [TestClass]
+    public class OrganizationControllerTest
+    {
+        private readonly OrganizationController _controller;
+        private readonly IOrganizationBusiness _business;
+
+     
+        public OrganizationControllerTest()
+        {
+            _business = new OrganizationBusinessTest();
+            _controller = new OrganizationController(_business);
+        }
+
+        [TestMethod]
+        public void GetById_ExistinOrgPassed_ReturnsOkResult()
+        {
+           
+            var id = 1;
+
+            var response = _controller.GetById(id);
+
+            Assert.IsNotNull(response);
+            
+        }
+
+        [TestMethod]
+        public void GetById_NonExistinOrgPassed_ReturnsNull()
+        {
+
+            var id = 5;
+
+            var response = _controller.GetById(id);
+
+            Assert.IsNull(response as JsonResult);
+        }
+
+        [TestMethod]
+        public void Update_ExistinOrgPassed_ReturnsOkResult()
+        {
+
+            var orgDto = new OrganizationDtoPostRequest();
+            orgDto.Id = 1;
+            orgDto.Name = "TestOrg1 MOD";
+            orgDto.Image = "Image\\Captura1.png";
+            orgDto.Address = "testAdressOrg1 MOD";
+            orgDto.Phone = "0303456nanana";
+            orgDto.Email = "MOD@testOrg1.com";
+            orgDto.WelcomeText = "welcome text org test 1 MOD";
+            orgDto.AboutUsText = "about us text org test 1 MOD";
+            orgDto.Facebook = "www.facebook.com/testOrg1";
+            orgDto.Linkedin = "www.linkedin.com/testOrg1";
+            orgDto.Instagram = "www.instagram.com/testOrg1";
+
+           
+            var okResult = _controller.updateOrganization(orgDto) as ObjectResult;
+
+            Assert.IsNotNull(okResult);
+        }
+
+        [TestMethod]
+        public void Update_NonExistinOrgPassed_ReturnsNull()
+        {
+
+            var orgDto = new OrganizationDtoPostRequest();
+            orgDto.Id = 22;
+            orgDto.Name = "TestOrg1 MOD";
+            orgDto.Image = "Image\\Captura1.png";
+            orgDto.Address = "testAdressOrg1 MOD";
+            orgDto.Phone = "0303456nanana";
+            orgDto.Email = "MOD@testOrg1.com";
+            orgDto.WelcomeText = "welcome text org test 1 MOD";
+            orgDto.AboutUsText = "about us text org test 1 MOD";
+            orgDto.Facebook = "www.facebook.com/testOrg1";
+            orgDto.Linkedin = "www.linkedin.com/testOrg1";
+            orgDto.Instagram = "www.instagram.com/testOrg1";
+
+            var okResult = _controller.updateOrganization(orgDto);
+
+            Assert.IsNull(okResult as JsonResult);
+        }
+
+    
+    }
+}
